a472f894a18495155f6ec312b2f498ae614f6d4f
Kateryna Hladka <pi60_gkv@student.ztu.edu.ua>
2022-11-10 15:01:32 +0200

    Created new console project and created abstract MusicalInstrument class

---
+++ Lab1ForNetCore/Classes/MusicalInstrument.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1ForNetCore.Classes
+{
+    public abstract class MusicalInstrument
+    {
+        public string Name { get; set; } = "";
+        public int Price { get; set; }
+
+        public abstract void Sound();
+        public abstract void Show();
+        public abstract void Decs();
+        public abstract void History();
+        public abstract void ShowInfo();
+        public object Clone() => MemberwiseClone();
+    }
+}
